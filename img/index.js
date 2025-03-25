59e57f113e546a76537718a59f2665017e0406e4
mhkmhsh <mmahesh12@gmail.com>
2025-03-25 14:31:25 -0400

    got the navbar outline done

---
+++ img/index.js
@@ -0,0 +1,6 @@
+document.getElementById("menu-btn").addEventListener("click", function() {
+    let sidebar = document.getElementById("sidebar");
+    let button = document.getElementById("menu-btn");
+    sidebar.classList.toggle("active");
+    button.classList.toggle("active");
+});
