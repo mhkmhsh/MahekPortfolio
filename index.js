a8c832205c96b608e8a2a07e11b24a2db4c649f9
mhkmhsh <mmahesh12@gmail.com>
2025-05-21 14:58:57 -0400

    working on this

--- index.js
+++ index.js
@@ -4,3 +4,38 @@ document.getElementById("menu-btn").addEventListener("click", function() {
     sidebar.classList.toggle("active");
     button.classList.toggle("active");
 });
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const lenis = new Lenis();
+    lenis.on("scroll", ScrollTrigger.update);
+    gsap.ticker.add((time)=>{
+        lenis.raf(time*1000);
+
+    });
+    gsap.ticker.lagSmoothing(0);
+
+const cardPositions = [
+{top:"30%" , left:"55%"}
+{top:"20%" , left:"25%"}
+{top:"50%" , left:"10%"}
+{top:"60%" , left:"40%"}
+{top:"30%" , left:"30%"}
+{top:"60%" , left:"60%"}
+{top:"20%" , left:"50%"}
+{top:"60%" , left:"10%"}
+{top:"20%" , left:"40%"}
+{top:"45%" , left:"55%"}
+];
+const imageContainer = document.querySelector(".images");
+for(let i = 1; i<=10; i++){
+    const card = document.createElement("div");
+    card.className=`card card-${i}`;
+
+    const img=document.createElement("img");
+    img.src = `./assets/img${i}.jpeg`
+}
+
+})
+https://www.youtube.com/watch?v=sRcH2BUatyQ
