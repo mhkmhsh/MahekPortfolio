45bb93c6346c1786790de67fae4ba865022aeb95
mhkmhsh <mahekmahesh12@gmail.com>
2025-05-24 14:19:28 -0400

    renamed images, trying to get the hero to work

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ document.getElementById("menu-btn").addEventListener("click", function () {
     sidebar.classList.toggle("active");
     button.classList.toggle("active");
 });
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+const interval = 2000;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
+}
+
+setInterval(() => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}, interval);
