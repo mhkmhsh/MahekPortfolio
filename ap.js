90278be6173c94e8b5b06789a7eeb34230583a38
mhkmhsh <mahekmahesh12@gmail.com>
2025-05-26 23:20:46 -0400

    working on the java

--- ap.js
+++ ap.js
@@ -14,5 +14,42 @@ function generateRandomName(){
 
 function generateRandomImageName(){
     const imageNumber = Math.floor(Math.random()*40)+1;
-    return `img${imageNumber}.png`
+    return `img${imageNumber}.png`;
+
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const gallaryContainer = document.querySelector(".gallery");
+    const imgModal =document.querySelector(".img-modal");
+    const imgViewContainer= imgModal.querySelector(".img");
+    const modalName = imgModal.querySelector(".img-name p");
+
+    const tl =gsap.timeline({paused:true});
+    let clickedItemImgSrc ="";
+    let clickedItemName = "";
+
+    for(let i =1; i <= 80; i++){
+        const item =document.createElement("div");
+        item.classList.add("item");
+
+        const itemImg = document.createElement("div");
+        itemImg.classList.add("item-img");
+
+        const imgTag= document.createElement("img");
+        const RandomImageName = generateRandomImageName();
+        imgTag.src= `./assets/${RandomImageName}`;
+        itemImg.appendChild(imgTag);
+
+        const itemName =document.createElement("div");
+        itemName.classList.add("item-name");
+        const randomName = generateRandomName();
+        itemName.innerHtml = `<p>${randomName}</p>`;
+        itemName.setAttribute("data-img", RandomImageName.replace(".png", ""));
+
+        item.appendChild(itemImg);
+        item.appendChild(itemName);
+
+        
+    }
+
+})
